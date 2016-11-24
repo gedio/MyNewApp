1dcc67418e861bd8c56222e522e2a4969cd562b0
Suma <Suma@suma-macbook-pro.local>
2016-11-24 16:49:39 +0800

    自定义背景颜色，导航栏，导航栏颜色，标题

--- MyNewApp/AppDelegate.m
+++ MyNewApp/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    ViewController *rootView = [[ViewController alloc] init];
+    UINavigationController *navigation = [[UINavigationController alloc] initWithRootViewController:rootView];
+    self.window.rootViewController = navigation;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
--- MyNewApp/ViewController.m
+++ MyNewApp/ViewController.m
@@ -17,6 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIColor *gray = [UIColor colorWithRed:0.95 green:0.95 blue:0.95 alpha:1];
+    self.view.backgroundColor = gray;
+    
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0.2 green:0.72 blue:0.46 alpha:1];
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+    
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 150, 44)];
+    titleLabel.text = @"2016年11月";
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.font = [UIFont boldSystemFontOfSize:17];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.backgroundColor = [UIColor clearColor];
+    
+    self.navigationItem.titleView = titleLabel;
+    
+    
+    
 }
 
 
