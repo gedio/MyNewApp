77250e89a4f795203e5ac6f3c4d100d1d261b241
Suma <Suma@suma-macbook-pro.local>
2016-11-24 14:39:46 +0800

    Initial Commit

---
+++ MyNewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNewApp
+//
+//  Created by Suma on 2016/11/24.
+//  Copyright © 2016年 Suma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyNewApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyNewApp
+//
+//  Created by Suma on 2016/11/24.
+//  Copyright © 2016年 Suma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
